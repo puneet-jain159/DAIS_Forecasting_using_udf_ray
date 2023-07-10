fc80004f5e52e5e626e998e45787bb8aac14b4da
puneet-jain159 <puneet.jain@databricks.com>
2023-07-10 13:19:52 +0100

    adding neuralprophet depedency

--- init_scripts/ray_install.sh
+++ init_scripts/ray_install.sh
@@ -5,6 +5,7 @@ RAY_PORT=9339
 # install ray
 # Install additional ray libraries
 /databricks/python/bin/pip install ray[tune,default]==2.5.1
+/databricks/python/bin/pip install neuralprophet
 
 #Create the location if not exists
 mkdir -p /local_disk0/tmp/ray/job
